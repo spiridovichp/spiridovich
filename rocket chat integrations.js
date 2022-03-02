8bdf8101ad663ddaec9f117b516bc5672173010c
spiridovichp <99966147+spiridovichp@users.noreply.github.com>
2022-03-02 11:11:17 +0300

    1

--- rocket chat integrations.js
+++ rocket chat integrations.js
@@ -46,3 +46,31 @@ class Script{
 				}
 			}
 		}
+
+		//test
+				class Script{
+						prepare_outgoing_request({request}){
+							let data = request.data.text;
+							let match = data.match(/(?<=\s)[\А-яA-z\s\,\\]+/g);
+							let json = JSON.stringify(request);
+							if (match.length > 0) {
+								return {
+									url: 'https://deep-sea.ru/rest/startIssue',
+									headers: request.headers,
+									method: 'POST',
+									data: {
+										name: match[0],
+										details: json,
+	 									started_by: request.data.user_name,
+										status: 'New',
+										issue_type: 'OTHER',
+										responsible: request.data.channel_name,
+										action: 'New'
+									 }
+								};
+							}
+						}
+					}
+
+
+{"params":{},"method":"POST","url":null,"data":{"token":"od0wt2zxt3c","bot":false,"trigger_word":"task","channel_id":"XQKTTBDJ4fvTNpXr9bckXgkNHP3c8hK8tX","message_id":"FDysLweN6azoJCMvy","timestamp":"2022-03-02T07:06:16.623Z","user_id":"bckXgkNHP3c8hK8tX","user_name":"spiridovich","text":"task test","siteUrl":"https://rocket.nautic-rus.ru"},"npmRequestOptions":{"rejectUnauthorized":false,"strictSSL":false},"headers":{"User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36"}}
